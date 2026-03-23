061975679ad0aba816c04f291b6426c7f589c443
suyn04 <63181309+suyn04@users.noreply.github.com>
2026-03-23 16:43:59 +0900

    원시타입과 리터럴타입

--- section1/dist/index.js
+++ section1/dist/index.js
@@ -1,12 +1,8 @@
 const a = 1;
-
 export {};
-
 // import { hello } from './hello';
 //
 // hello();
-
 // const func = () => console.log('Hello');
-
 // console.log('Hello Typescript');
 // const a: number = 1;
